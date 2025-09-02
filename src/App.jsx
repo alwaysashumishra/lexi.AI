c061817d7d6760dc0e3efc84a6947060b75f9bfe
Ashutosh <ashutoshmmishra15@gmail.com>
2025-09-02 18:20:20 +0530

    mes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/Main'
-
+import React, { useState, useEffect } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+import Preloader from "./components/Preloader/Preloader";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2500); // 2.5 sec
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-    <Sidebar/>
-    <Main/>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <>
+          <Sidebar />
+          <Main />
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
